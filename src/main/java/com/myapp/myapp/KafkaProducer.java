5421bc92a8d19ca5c7e5b8cdb9e6f86977329bca
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-13 15:27:05 +0500

    KafkaProducer: Added @Component annotation KafkaProducer: Created new sendMessage method KafkaProducer: Added @Bean, @Transactional annotation

--- src/main/java/com/myapp/myapp/KafkaProducer.java
+++ src/main/java/com/myapp/myapp/KafkaProducer.java
@@ -1,4 +1,16 @@
 package com.myapp.myapp;
 
+import jakarta.transaction.Transactional;
+import jakarta.transaction.TransactionalException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
 public class KafkaProducer {
+
+    @Bean
+    @Transactional
+    public void sendMessages() throws TransactionalException, InterruptedException {
+
+    }
 }
