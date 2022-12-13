526c7e783a408e30b68e8ae0d322537807962060
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-13 16:11:14 +0500

    TransactionEmitter: Added @RequiredArgsConstructor annotation TransactionEmitter: Injected KafkaProducer dependency

--- src/main/java/com/myapp/myapp/TransactionEmitter.java
+++ src/main/java/com/myapp/myapp/TransactionEmitter.java
@@ -1,7 +1,10 @@
 package com.myapp.myapp;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class TransactionEmitter {
+    private final KafkaProducer kafkaProducer;
 }
