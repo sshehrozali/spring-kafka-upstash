5cc3d499bafa479da97401ee3882d78d6c3e1342
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-13 17:26:32 +0500

    MyappApplication: Injected TransactionEmitterService dependency MyappApplication: Added @RequiredArgsConstructor annotation

--- src/main/java/com/myapp/myapp/MyappApplication.java
+++ src/main/java/com/myapp/myapp/MyappApplication.java
@@ -1,5 +1,6 @@
 package com.myapp.myapp;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +8,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class MyappApplication {
+
+	private final TransactionEmitterService transactionEmitterService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyappApplication.class, args);
