a3460e3bc4c32ebd23cb6dee4c45b4d633b0aed5
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-13 15:32:06 +0500

    Transaction: Added @Entity, @AllArgsConstructor, @NoArgsConstructor, @Getter, @Setter, @Builder Transaction: Implemented fields Transaction: Added @Id, @GeneratedValue annotations to id field

--- src/main/java/com/myapp/myapp/Transaction.java
+++ src/main/java/com/myapp/myapp/Transaction.java
@@ -1,2 +1,21 @@
-package com.myapp.myapp;public class Transaction {
+package com.myapp.myapp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String transaction_id;
+    private String amount;
 }
