8b0e8a3c89094a7dcd069c1597cd3f583f579e15
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-13 17:16:31 +0500

    TransactionEmitterService: Refactored name to TransactionEmitterService

--- src/main/java/com/myapp/myapp/TransactionEmitterService.java
+++ src/main/java/com/myapp/myapp/TransactionEmitterService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class TransactionEmitter {
+public class TransactionEmitterService {
     private final KafkaProducer kafkaProducer;
 }
