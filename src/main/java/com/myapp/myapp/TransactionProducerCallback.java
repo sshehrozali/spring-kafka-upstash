9ee9d0581093e7001b3b6c0012944ab163ceefcf
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-13 15:45:48 +0500

    TransactionProducerCallback: class ListenableFutureCallback<SendResult<Integer, Transaction>> TransactionProducerCallback: Implemented override methods

--- src/main/java/com/myapp/myapp/TransactionProducerCallback.java
+++ src/main/java/com/myapp/myapp/TransactionProducerCallback.java
@@ -1,7 +1,20 @@
 package com.myapp.myapp;
 
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @Component
-public class TransactionProducerCallback {
+public class TransactionProducerCallback implements ListenableFutureCallback<SendResult<Integer, Transaction>> {
+
+    @Override
+    public void onFailure(Throwable ex) {
+
+    }
+
+    @Override
+    public void onSuccess(SendResult<Integer, Transaction> result) {
+
+    }
 }
