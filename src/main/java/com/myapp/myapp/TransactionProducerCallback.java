b3c5b49897e50cf625a36548daacce48c1d88b20
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-13 15:51:44 +0500

    TransactionProducerCallback: Added @Slf4j annotation TransactionProducerCallback: Implemented logging inside onFailure and onSuccess methods

--- src/main/java/com/myapp/myapp/TransactionProducerCallback.java
+++ src/main/java/com/myapp/myapp/TransactionProducerCallback.java
@@ -1,20 +1,24 @@
 package com.myapp.myapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @Component
+@Slf4j
 public class TransactionProducerCallback implements ListenableFutureCallback<SendResult<Integer, Transaction>> {
 
     @Override
     public void onFailure(Throwable ex) {
-
+        log.error("Sent Message failed");
     }
 
     @Override
     public void onSuccess(SendResult<Integer, Transaction> result) {
-
+        if (result != null) {
+            log.info("Send Message Success" + result.getProducerRecord().key() + result.getProducerRecord().value());
+        }
     }
 }
