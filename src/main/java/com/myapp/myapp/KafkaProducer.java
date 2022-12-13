8505705345a499865377e0c1588704bfeca35f51
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-13 15:38:15 +0500

    KafkaProducer: Injected KafkaTemplate dependency KafkaProducer: Added @RequiredArgsConstructor annotation to kafkaTemplate field

--- src/main/java/com/myapp/myapp/KafkaProducer.java
+++ src/main/java/com/myapp/myapp/KafkaProducer.java
@@ -2,15 +2,24 @@ package com.myapp.myapp;
 
 import jakarta.transaction.Transactional;
 import jakarta.transaction.TransactionalException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
 
 @Component
+@RequiredArgsConstructor
 public class KafkaProducer {
 
+    private final KafkaTemplate kafkaTemplate;
     @Bean
     @Transactional
     public void sendMessages() throws TransactionalException, InterruptedException {
-
+        Transaction t = Transaction.builder()
+                .transaction_id("AAA-123")
+                .amount("500")
+                .build();
     }
 }
