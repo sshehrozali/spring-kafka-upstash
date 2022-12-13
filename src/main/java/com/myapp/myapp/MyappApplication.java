ab2d151f3569ae323b142fec30d297a625805f02
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-13 14:48:19 +0500

    MyappApplication: Implemented newTopic method that creates a new Kafka topic 'transactions-topic' MyappApplication: Added @Bean annotation to newTopic method

--- src/main/java/com/myapp/myapp/MyappApplication.java
+++ src/main/java/com/myapp/myapp/MyappApplication.java
@@ -1,13 +1,24 @@
 package com.myapp.myapp;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
 public class MyappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyappApplication.class, args);
+	}
+
+	@Bean
+	public NewTopic newTopic() {
+		return TopicBuilder.name("transactions-topic")
+				.partitions(3)
+				.replicas(1)
+				.build();
 	}
 
 }
