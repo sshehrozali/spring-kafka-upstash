cb543f9c9c3c241ad397878e8a035dc0f435632f
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-14 11:07:00 +0500

    TransactionRepository: Added @Repository annotation

--- src/main/java/com/myapp/myapp/TransactionRepository.java
+++ src/main/java/com/myapp/myapp/TransactionRepository.java
@@ -1,6 +1,8 @@
 package com.myapp.myapp;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Integer, Transaction> {
 }
