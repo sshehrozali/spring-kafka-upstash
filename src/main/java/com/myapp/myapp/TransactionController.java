dd566b633e901c724e766d7785b70d7a0c0ec0a7
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-16 11:32:45 +0500

    TransactionController: Injected TransactionEmitterService dependency

--- src/main/java/com/myapp/myapp/TransactionController.java
+++ src/main/java/com/myapp/myapp/TransactionController.java
@@ -1,5 +1,6 @@
 package com.myapp.myapp;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,7 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1")
+@RequiredArgsConstructor
 public class TransactionController {
+    private final TransactionEmitterService transactionEmitterService;
     @PostMapping("/new")
     public ResponseEntity<TransactionResponse> newTransaction() {
 
