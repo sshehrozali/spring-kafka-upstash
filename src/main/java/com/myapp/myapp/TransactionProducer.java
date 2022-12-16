2d87b30aea347a0124e9fc67891700b1576f579b
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-16 12:56:40 +0500

    TransactionProducer: Swapped TransactionalException to TransactionException

--- src/main/java/com/myapp/myapp/TransactionProducer.java
+++ src/main/java/com/myapp/myapp/TransactionProducer.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.TransactionException;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.concurrent.ListenableFuture;
 
@@ -16,7 +17,7 @@ public class TransactionProducer {
     private final TransactionProducerCallback transactionProducerCallback;
 
     @Transactional
-    public void sendTransaction(Transaction t) throws TransactionalException, InterruptedException {
+    public void sendTransaction(Transaction t) throws InterruptedException, TransactionException {
         ListenableFuture<SendResult<Integer, Transaction>> result = (ListenableFuture<SendResult<Integer, Transaction>>) kafkaTemplate.send("transactions", t.getTransaction_id(), t);
         result.addCallback(transactionProducerCallback);
         Thread.sleep(1000);
