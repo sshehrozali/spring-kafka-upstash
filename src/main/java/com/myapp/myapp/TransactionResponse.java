0aeeb93831666270c27fd4041b74f8875b5b852d
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-16 11:28:38 +0500

    TransactionResponse: Added fields

--- src/main/java/com/myapp/myapp/TransactionResponse.java
+++ src/main/java/com/myapp/myapp/TransactionResponse.java
@@ -1,7 +1,11 @@
 package com.myapp.myapp;
 
 import lombok.Builder;
+import org.springframework.http.HttpStatus;
 
 @Builder
-public record TransactionResponse() {
+public record TransactionResponse(
+        HttpStatus httpStatus,
+        String msg
+) {
 }
