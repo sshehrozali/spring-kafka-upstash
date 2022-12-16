69d98701706a8257cbbeb73c7fce29451f1e877c
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-16 11:38:34 +0500

    TransactionController: Added @RequestBody parameter to newTransaction TransactionController: Passed newTransactionRequest to processTransaction

--- src/main/java/com/myapp/myapp/TransactionController.java
+++ src/main/java/com/myapp/myapp/TransactionController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TransactionController {
     private final TransactionEmitterService transactionEmitterService;
     @PostMapping("/new")
-    public ResponseEntity<TransactionResponse> newTransaction() {
-
+    public ResponseEntity<TransactionResponse> newTransaction(@RequestBody NewTransactionRequest newTransactionRequest) {
+        transactionEmitterService.processTransaction(newTransactionRequest);
     }
 }
