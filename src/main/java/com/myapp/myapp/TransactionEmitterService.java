805e5f44898be90b571ad1185e6b0ef92eb08d6a
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-16 11:12:28 +0500

    TransactionEmitterService: Added @Slf4j annotation

--- src/main/java/com/myapp/myapp/TransactionEmitterService.java
+++ src/main/java/com/myapp/myapp/TransactionEmitterService.java
@@ -1,15 +1,17 @@
-//package com.myapp.myapp;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class TransactionEmitterService {
-//    private final TransactionRepository transactionRepository;
-//    private final KafkaProducer kafkaProducer;
-//
-//    public void processTransaction() throws InterruptedException {
-//        kafkaProducer.sendMessages();
-//    }
-//}
+package com.myapp.myapp;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class TransactionEmitterService {
+    private final TransactionRepository transactionRepository;
+    private final KafkaProducer kafkaProducer;
+
+    public void processTransaction() throws InterruptedException {
+        kafkaProducer.sendMessages();
+    }
+}
