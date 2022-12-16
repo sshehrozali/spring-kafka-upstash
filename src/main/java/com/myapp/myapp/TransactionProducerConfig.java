68daf28090ffcde13ac9c327f6f2d7a2f11de08b
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-16 14:20:11 +0500

    TransactionProducerConfig: Added @Bean annotation to kafkaTransactionManager method

--- src/main/java/com/myapp/myapp/TransactionProducerConfig.java
+++ src/main/java/com/myapp/myapp/TransactionProducerConfig.java
@@ -2,6 +2,7 @@ package com.myapp.myapp;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.transaction.KafkaTransactionManager;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @Configuration
 public class TransactionProducerConfig {
+    @Bean
     KafkaTransactionManager<Integer, Transaction> kafkaTransactionManager(final ProducerFactory<Integer, Transaction> producerFactory) {
         return new KafkaTransactionManager<>(producerFactory);
     }
