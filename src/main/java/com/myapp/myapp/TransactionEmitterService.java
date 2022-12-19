900d3b4cc52b49b688e9f7b2cd3e19b0ddc9a1b3
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-19 14:44:22 +0500

    TransactionEmitterService: Set id of Transaction t in builder

--- src/main/java/com/myapp/myapp/TransactionEmitterService.java
+++ src/main/java/com/myapp/myapp/TransactionEmitterService.java
@@ -4,24 +4,32 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class TransactionEmitterService {
-    private final TransactionRepository transactionRepository;
+//    private final TransactionRepository transactionRepository;
     private final TransactionProducer transactionProducer;
 
     public CustomTransactionResponse processTransaction(NewTransactionRequest newTransactionRequest) throws InterruptedException {
         Transaction t = Transaction.builder()
+                .id(1)
                 .transaction_id("ABC-123")
                 .amount(newTransactionRequest.amount())
+                .state(0)   // Default to 0
                 .build();
-        log.warn("Sending Transaction... " + t);
-        transactionProducer.sendTransaction(t);
-        log.info("Sent");
+//        transactionRepository.save(t);
+        log.warn("Transaction initiated successfully");
 
-        transactionRepository.save(t);
+//        log.warn("Sending Transaction... " + t_initiated);
+//        transactionProducer.sendTransaction(t_initiated);
+//        log.info("Sent");
+//
+//        t_initiated.setState(1);    // Set Transaction to success
+//        Transaction t_success = t_initiated;    // Assign t again
+//        transactionRepository.saveAndFlush(t_success);  // Rewrite on db
 
         CustomTransactionResponse customTransactionResponse = CustomTransactionResponse.builder()
                 .httpStatus(HttpStatus.OK)
