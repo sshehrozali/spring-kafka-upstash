cbffc8d778e9d1da5474d1882efa0df4e015adf4
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-20 18:38:33 +0500

    TransactionProducer: Added @Transactional annotation to sendTransaction

--- src/main/java/com/myapp/myapp/TransactionProducer.java
+++ src/main/java/com/myapp/myapp/TransactionProducer.java
@@ -3,6 +3,7 @@ package com.myapp.myapp;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -10,6 +11,7 @@ public class TransactionProducer {
     private final KafkaTemplate<String, Object> kafkaTemplate;
     private final TransactionProducerCallback transactionProducerCallback;
 
+    @Transactional
     public void sendTransaction(Transaction t) {
         kafkaTemplate.send("transactions-topic", t);
     }
