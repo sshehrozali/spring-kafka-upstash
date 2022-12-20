274a2a05168c2228b04a886b4be5114e282fa3b4
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-20 17:36:17 +0500

    TransactionProducer: Removed unused libraries

--- src/main/java/com/myapp/myapp/TransactionProducer.java
+++ src/main/java/com/myapp/myapp/TransactionProducer.java
@@ -1,19 +1,10 @@
 package com.myapp.myapp;
 
-import jakarta.transaction.TransactionalException;
 import lombok.RequiredArgsConstructor;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.TransactionException;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.concurrent.ListenableFuture;
-
-import java.util.Objects;
-import java.util.Properties;
 
 @Component
 @RequiredArgsConstructor
