1ce0edb46ea1fc5f9c371c5ae0e40f67c629e3c9
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-20 18:37:00 +0500

    TransactionEmitterService: Refactored

--- src/main/java/com/myapp/myapp/TransactionEmitterService.java
+++ src/main/java/com/myapp/myapp/TransactionEmitterService.java
@@ -10,26 +10,18 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Slf4j
 public class TransactionEmitterService {
-//    private final TransactionRepository transactionRepository;
     private final TransactionProducer transactionProducer;
 
     public CustomTransactionResponse processTransaction(NewTransactionRequest newTransactionRequest) throws InterruptedException {
         Transaction t = Transaction.builder()
-                .id(1)
                 .transaction_id("ABC-123")
                 .amount(newTransactionRequest.amount())
                 .state(0)   // Default to 0
                 .build();
-//        transactionRepository.save(t);
-        log.warn("Transaction initiated successfully");
 
-//        log.warn("Sending Transaction... " + t_initiated);
-//        transactionProducer.sendTransaction(t_initiated);
-//        log.info("Sent");
-//
-//        t_initiated.setState(1);    // Set Transaction to success
-//        Transaction t_success = t_initiated;    // Assign t again
-//        transactionRepository.saveAndFlush(t_success);  // Rewrite on db
+        log.warn("Transaction initiated successfully");
+        transactionProducer.sendTransaction(t);
+        log.warn("Transaction sent successfully");
 
         CustomTransactionResponse customTransactionResponse = CustomTransactionResponse.builder()
                 .httpStatus(HttpStatus.OK)
