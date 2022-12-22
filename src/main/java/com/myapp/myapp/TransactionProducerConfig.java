514f5ecba9ad5547fba6ec49926c8c9588c10ec3
Shehroz Ali <shehroz.ali@gopayfast.com>
2022-12-22 13:13:30 +0500

    TransactionProducerConfig: Added newTopic method for creating Kafka topic

--- src/main/java/com/myapp/myapp/TransactionProducerConfig.java
+++ src/main/java/com/myapp/myapp/TransactionProducerConfig.java
@@ -1,9 +1,11 @@
 package com.myapp.myapp;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.transaction.KafkaTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -11,6 +13,13 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @Configuration
 public class TransactionProducerConfig {
+    @Bean
+    public NewTopic newTopic() {
+        return TopicBuilder.name("transactions-topic")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
     @Bean(name = "transactionManager")
     KafkaTransactionManager<Integer, Transaction> kafkaTransactionManager(final ProducerFactory<Integer, Transaction> producerFactory) {
         return new KafkaTransactionManager<>(producerFactory);
